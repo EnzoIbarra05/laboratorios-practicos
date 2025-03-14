897f4d3f7c72f8c02b647fd716cde92c6a7ae8a8
SantiagoVidela933 <santi.videla.2@gmail.com>
2025-03-14 17:53:49 -0300

    Modifico fetchs con try-catch

--- Tp-1-labo/codigo.js
+++ Tp-1-labo/codigo.js
@@ -1,34 +1,33 @@
-function loginPersonal() {
-    // DATOS
+document.getElementById('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-    let usuario = document.getElementById("usuario").value;
-    let contrasenia = document.getElementById("contrasenia").value;
-    
-    // URL contraseña y usuario
+    let usuario = document.getElementById("usuario").value.trim();
+    let contrasenia = document.getElementById("contrasenia").value.trim();
+    let mensaje = document.getElementById("mensaje");
 
-    let url = `http://181.111.166.250:8081/tp/login.php?user=${usuario}&pass=${contrasenia}`;
+    if (!usuario || !contrasenia) {
+        mensaje.style.color = "red";
+        mensaje.textContent = "Usuario y Contraseña son obligatorios";
+        return;
+    }
 
-    // Hacer la petición GET
-    fetch(url)
-        .then(response => response.json()) 
-        .then(data => {
-           
-            let mensaje = document.getElementById("mensaje");
-            if (data.respuesta === "OK") {
-                mensaje.style.color = "green";
-                mensaje.textContent = data.mje + ", Redireccionando...";
-               
-                //Tiempo para que se abra la nueva pestaña 
-                
-                setTimeout(()=>{
-                window.location.href="lista.html"},3000);
-                
-            } else {
-                mensaje.style.color = "red";
-                mensaje.textContent = data.mje;
-            }
-        })
-        .catch(error => console.error("Error en la petición:", error));
-}
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/login.php?user=${usuario}&pass=${contrasenia}`);
+        const data = await response.json();
 
+        if (data.respuesta === "OK") {
+            mensaje.style.color = "green";
+            mensaje.textContent = `${data.mje}, Redireccionando...`;
+
+            setTimeout(() => window.location.href = "lista.html", 3000);
+        } else {
+            mensaje.style.color = "red";
+            mensaje.textContent = data.mje;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        mensaje.style.color = "red";
+        mensaje.textContent = "Error en la conexión";
+    }
+});
 
--- Tp-1-labo/listas.js
+++ Tp-1-labo/listas.js
@@ -1,28 +1,22 @@
-// URL de la API de usuarios
+// Se llama a la funcion cargar datos al cargar el html/DOM
+document.addEventListener("DOMContentLoaded", cargarDatos);
 
-const urlCarga = "http://181.111.166.250:8081/tp/lista.php?action=BUSCAR";
+// Funcion para cargar todos los usuarios desde un principio
+async function cargarDatos() {
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BUSCAR`);
+        const data = await response.json();
 
+        //llamo a la tabla a completar
+        let tablaBody = document.getElementById("tabla-body");
 
-//Funcion para cargar todos los usuarios desde un principio
+        //Limpio la tabla por si ya tenia datos
+        tablaBody.innerHTML = ""; 
 
-function cargarDatos() {
-
-    fetch(urlCarga) 
-        .then(response => response.json()) 
-        .then(data => {
-            //llamo a la tabla a completar
-
-            let tablaBody = document.getElementById("tabla-body");
-
-            //Limpio la tabla por si ya tenia datos
-
-            tablaBody.innerHTML = ""; 
-
-            // Recorro el JSON e inserto la info
-
-            Object.values(data).forEach(persona => {
-                let fila = document.createElement("tr");
-                fila.innerHTML = `
+        // Recorro el JSON e inserto la info
+        Object.values(data).forEach(persona => {
+            let fila = document.createElement("tr");
+            fila.innerHTML = `
                 <td>${persona.id}</td>
                 <td>${persona.usuario}</td>
                 <td>${persona.bloqueado}</td>
@@ -33,112 +27,92 @@ function cargarDatos() {
             `;
 
             // Verifico el color de la fila si esta bloqueado o no
-
-                if(persona.bloqueado==="N"){
-                fila.style.backgroundColor="#cef8c6";
-                }
-                else{
-                    fila.style.backgroundColor="#fd9f8b";
-                }
+            fila.style.backgroundColor = persona.bloqueado === "N" ? "#cef8c6" : "#fd9f8b";
             
             //inserto fila
-
-                tablaBody.appendChild(fila);
-            });
-        })
-        .catch(error => console.error("Error al obtener datos:", error)); // Capturar errores
+            tablaBody.appendChild(fila);
+        });
+    } catch (error) {
+        console.error("Error al obtener datos:", error)
+    }
 }
 
-window.onload = cargarDatos;
+// Función flecha al escuchar el 'submit' del form. de busqueda
+document.getElementById('formBuscar').addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    const busqueda = document.getElementById("buscando").value.trim();
 
-function buscar() {
-   
-    const busqueda = document.getElementById("buscando").value;
+    if (!busqueda) {alert("Ingrese un usuario")};
 
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BUSCAR&usuario=${busqueda}`);
+        const data = await response.json();
+        
+        let tablaBody = document.getElementById("tabla-body");
+        tablaBody.innerHTML = "";
 
-    const urlBusqueda = `http://181.111.166.250:8081/tp/lista.php?action=BUSCAR&usuario=${busqueda}`;
-
-    fetch(urlBusqueda)
-        .then(response => response.json())
-        .then(data => {
-            let tablaBody = document.getElementById("tabla-body");
-            tablaBody.innerHTML = "";
-
-            // Verifico si hay 'data' disponible
-
-            if (Object.values(data).length === 0) {
-                alert("No se encontraron resultados.");
-                cargarDatos();
-
-            } else {
-            
+        // Verifico si hay 'data' disponible
+        if (Object.values(data).length === 0) {
+            alert("No se encontraron resultados.");
+            cargarDatos();
+        } else {
             //si la hay devuelvo lo encontrado
-
-                Object.values(data).forEach(persona => {
-                    let fila = document.createElement("tr");
-                    fila.innerHTML = `
-                    <td>${persona.id}</td>
-                    <td id="${persona.usuario}">${persona.usuario}</td>
-                    <td>${persona.bloqueado}</td>
-                    <td>${persona.nombre}</td>
-                    <td>${persona.apellido}</td>
-                    <td><button onclick="bloquear('${persona.id}')" style="background-color: red;color:white">Bloquear</button></td>
-                    <td><button onclick="desbloquear('${persona.id}')" style="background-color: green;color:white">Desbloquear</button></td>
+            Object.values(data).forEach(persona => {
+                let fila = document.createElement("tr");
+                fila.innerHTML = `
+                <td>${persona.id}</td>
+                <td id="${persona.usuario}">${persona.usuario}</td>
+                <td>${persona.bloqueado}</td>
+                <td>${persona.nombre}</td>
+                <td>${persona.apellido}</td>
+                <td><button onclick="bloquear('${persona.id}')" style="background-color: red;color:white">Bloquear</button></td>
+                <td><button onclick="desbloquear('${persona.id}')" style="background-color: green;color:white">Desbloquear</button></td>
                 `;
 
                 //aqui tambien verifico color segun el estado de bloqueo
-
-                    if(persona.bloqueado==="N"){
+                fila.style.backgroundColor = persona.bloqueado === "N" ? "#cef8c6" : "#fd9f8b";
                 
-                        fila.style.backgroundColor="#cef8c6";
-                }
-                    else{
-                        fila.style.backgroundColor="#fd9f8b";
-                    }
-                  
-                    tablaBody.appendChild(fila);
-                });
-            }
-        })
-        .catch(error => console.error("Error al hacer la solicitud:", error));
-}
+                tablaBody.appendChild(fila);
+            });
+        }
+    } catch (error) {
+        console.error("Error al hacer la solicitud:", error)
+    }
+});
 
-
-
-function bloquear(Idusuario) { 
-    //Tomo el id de la persona a bloquear
-
+// Función para bloquear usuario
+async function bloquear(Idusuario) { 
     console.log("ID del usuario a bloquear:", Idusuario);
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${Idusuario}&estado=Y`);
+        const data = await response.json();
 
-    //creo URL de bloqueo segun Id
-    let urlBloqueo = `http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${Idusuario}&estado=Y`;
+        console.log("Respuesta de la API:", response);
+        console.log("Respuesta JSON:", data);
 
-    //realizo la peticion
-    
-    fetch(urlBloqueo)
-        .then(response => {
-            console.log("Respuesta de la API:", response);
-            return response.json();
-        })
-        .then(data => {
-            console.log("Respuesta JSON:", data);
-        })
-        .catch(error => console.error("Error al bloquear:", error));
+        if (data.respuesta === "OK") {
+            cargarDatos();
+        }
+        
+    } catch (error) {
+        console.error("Error al bloquear:", error)
+    }
 }
 
-
-function desbloquear(Idusuario) { 
+// Función para desbloquear usuario
+async function desbloquear(Idusuario) { 
     console.log("ID del usuario a desbloquear:", Idusuario);
-    let urlDesbloqueo = `http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${Idusuario}&estado=N`;
-    fetch(urlDesbloqueo)
-        .then(response => {
-            console.log("Respuesta de la API:", response);
-            return response.json();
-    
-        })
-        .then(data => {
-            console.log("Respuesta JSON:", data);
-        })
-        .catch(error => console.error("Error al bloquear:", error));
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${Idusuario}&estado=N`);
+        const data = await response.json();
+        console.log("Respuesta de la API:", response);
+        console.log("Respuesta JSON:", data);
+
+        if (data.respuesta === "OK") {
+            cargarDatos(); 
+        }
+    } catch (error) {
+        console.error("Error al bloquear:", error)
+    }
 }
