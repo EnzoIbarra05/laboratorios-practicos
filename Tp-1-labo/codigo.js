e353b3c10a3dc762c7b7d2c8d2aab49d59383a9c
betMartin <betsabemartin@gmail.com>
2025-03-14 20:29:19 -0300

    Pase los eventos del DOM a codigo.js

--- Tp-1-labo/codigo.js
+++ Tp-1-labo/codigo.js
@@ -1,34 +1,33 @@
-function loginPersonal() {
-    // DATOS
 
-    let usuario = document.getElementById("usuario").value;
-    let contrasenia = document.getElementById("contrasenia").value;
+//Cargar eventos del DOM--
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('formularioIngreso');
+    formulario.addEventListener('submit', validarIngreso);
+});
+
+
+//Función para validar ingreso de usuario--
+async function validarIngreso(event) {
+    event.preventDefault();
     
-    // URL contraseña y usuario
+    const usuario = document.getElementById('usuario').value;
+    const clave = document.getElementById('clave').value;
+    
 
-    let url = `http://181.111.166.250:8081/tp/login.php?user=${usuario}&pass=${contrasenia}`;
+    const url = `http://181.111.166.250:8081/tp/login.php?user=${encodeURIComponent(usuario)}&pass=${encodeURIComponent(clave)}`;
+    
+    try{
+        const rtaObtenida = await fetch(url);
+        const dato = await rtaObtenida.json();
 
-    // Hacer la petición GET
-    fetch(url)
-        .then(response => response.json()) 
-        .then(data => {
-           
-            let mensaje = document.getElementById("mensaje");
-            if (data.respuesta === "OK") {
-                mensaje.style.color = "green";
-                mensaje.textContent = data.mje + ", Redireccionando...";
-               
-                //Tiempo para que se abra la nueva pestaña 
-                
-                setTimeout(()=>{
-                window.location.href="lista.html"},3000);
-                
-            } else {
-                mensaje.style.color = "red";
-                mensaje.textContent = data.mje;
-            }
-        })
-        .catch(error => console.error("Error en la petición:", error));
+        if (dato.respuesta === "OK") {
+            alert(dato.mje);
+            window.location.href = "lista.html"; 
+        } else {
+            alert(dato.mje);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al procesar la solicitud. Intente nuevamente.');
+    }
 }
-
-
